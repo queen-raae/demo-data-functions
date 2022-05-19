8c6d9fcead4ac21d2a94d65f1a304da6c44db840
Benedicte Raae <raae@raae.codes>
2022-05-19 21:31:28 +0200

    Moved dotenv

--- src/api/hello-raw.js
+++ src/api/hello-raw.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
